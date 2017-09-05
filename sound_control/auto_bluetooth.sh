fafd55dadab6d02b90beb3333897a91591a3ceb7
Nathan Harrington <nharrington@wasatchphotonics.com>
2017-09-05 10:04:20 -0400

    sound control updates

--- sound_control/auto_bluetooth.sh
+++ sound_control/auto_bluetooth.sh
@@ -3,7 +3,10 @@
 # Bose QuietControl 30 on Fedora Core 24
 # In /etc/bluetooth/main.conf, make sure to set:
 # ControllerMode = bredr
-# Pair with blueman-manager or bluetoothctl manually 
+# Pair with blueman-manager or bluetoothctl manually first, then run
+# this on every reboot
+# Use the pulsemixer from: https://github.com/GeorgeFilipkin/pulsemixer
+# to control sound levels on command line like alsamixer
 
 # Create an input stream for the bluetoothctrl command
 mkfifo /tmp/srv-input 
@@ -12,7 +15,13 @@ tail -f /tmp/srv-input | bluetoothctl &
 # Loop forever, attempt to connect every 2 seconds and set the sink
 while [ true ]
 do
-echo  "connect 04:52:C7:1B:D7:F7" > /tmp/srv-input
-pactl set-default-sink bluez_sink.04_52_C7_1B_D7_F7
+# u430 touch with in-ears
+#echo  "connect 04:52:C7:1B:D7:F7" > /tmp/srv-input
+#pactl set-default-sink bluez_sink.04_52_C7_1B_D7_F7
+
+# Zenbook with bose over-ears
+echo  "connect 04:52:C7:35:09:EF" > /tmp/srv-input
+pactl set-default-sink bluez_sink.04_52_C7_35_09_EF
+
 sleep 2
 done
