ba3e9ceee7dac7df97bb0d4f7c1f9eec42e484b0
Nathan Harrington <nharrington@wasatchphotonics.com>
2017-10-17 14:26:12 -0400

    change to full intrakey delay with no clipboard

---
+++ autokeyboard/w3m_google.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# Create a shortcut in gnome mapped to ctrl-shift-g, then make sure the
+# w3m window is focused.
+#
+# TODO: Verify that the w3m application has the input
+#
+INTRA_KEY_DELAY=0.05
+# Open new tab
+sleep 0.1
+xdotool key "T"
+
+# Goto url
+sleep INTRA_KEY_DELAY
+xdotool key "U"
+
+# Clear URL bar, type google, press enter
+sleep INTRA_KEY_DELAY
+xdotool key ctrl+u
+sleep INTRA_KEY_DELAY
+xdotool key g
+sleep INTRA_KEY_DELAY
+xdotool key o
+sleep INTRA_KEY_DELAY
+xdotool key o
+sleep INTRA_KEY_DELAY
+xdotool key g
+sleep INTRA_KEY_DELAY
+xdotool key l
+sleep INTRA_KEY_DELAY
+xdotool key e
+sleep INTRA_KEY_DELAY
+xdotool key period
+sleep INTRA_KEY_DELAY
+xdotool key c
+sleep INTRA_KEY_DELAY
+xdotool key o
+sleep INTRA_KEY_DELAY
+xdotool key m
+sleep INTRA_KEY_DELAY
+xdotool key Return
+
+# 13 tabs', then press enter
+sleep 0.5
+xdotool key Tab
+xdotool key Tab
+xdotool key Tab
+xdotool key Tab
+xdotool key Tab
+xdotool key Tab
+xdotool key Tab
+xdotool key Tab
+xdotool key Tab
+xdotool key Tab
+xdotool key Tab
+xdotool key Tab
+xdotool key Tab
+xdotool key Return
