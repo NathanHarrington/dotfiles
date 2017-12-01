5090d14c538c03b1a0ce9855665751aa64d2a626
Nathan Harrington <harrington.nathan@gmail.com>
2017-12-01 09:40:21 -0500

    full screen bluetooth reset

---
+++ sound_control/fullscreen_bluetooth.sh
@@ -0,0 +1,6 @@
+# Add this to a gnome keyboard shortcut of ctrl-alt-b, with the command:
+# /usr/bin/xterm -fg green -bg black +sb -fullscreen \
+#    /home/nharrington/projects/dotfiles/sound_control/bluetooth.sh
+
+figlet "Re-connect bluetooth"
+/home/nharrington/projects/dotfiles/sound_control/auto_bluetooth.sh
