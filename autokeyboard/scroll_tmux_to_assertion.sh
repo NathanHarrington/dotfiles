635ecb36eca14c28aac3af964853ecd865f8463e
Nathan Harrington <harrington.nathan@gmail.com>
2017-12-02 10:25:52 -0500

    scroll tmux

---
+++ autokeyboard/scroll_tmux_to_assertion.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# Create a shortcut in gnome mapped to ctrl-shift-6, then make sure the
+# tmux window is focused.
+#
+INTRA_KEY_DELAY=0.05
+
+# Do the command mode, buffer scroll, search backwards, look for python
+# error, then press enter 
+xdotool key ctrl+a
+sleep INTRA_KEY_DELAY
+
+xdotool type [
+sleep INTRA_KEY_DELAY
+
+xdotool type ?
+sleep INTRA_KEY_DELAY
+
+xdotool type AssertionError
+sleep INTRA_KEY_DELAY
+
+xdotool key Return
+
