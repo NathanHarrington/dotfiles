1fbebc9f3236a4017c095006b59a1ca9e4071c32
Nathan Harrington <harrington.nathan@gmail.com>
2018-01-18 16:50:50 -0500

    conversion to single quotes and gnome-terminal

--- sound_control/fullscreen_bluetooth.sh
+++ sound_control/fullscreen_bluetooth.sh
@@ -1,6 +1,6 @@
 # Add this to a gnome keyboard shortcut of ctrl-alt-b, with the command:
-# /usr/bin/xterm -fg green -bg black +sb -fullscreen \
-#    /home/nharrington/projects/dotfiles/sound_control/bluetooth.sh
+# /usr/bin/gnome-terminal --full-screen -- sh -c \
+#    '/home/nharrington/projects/dotfiles/sound_control/fullscreen_bluetooth.sh'
 
 figlet "Re-connect bluetooth"
 /home/nharrington/projects/dotfiles/sound_control/auto_bluetooth.sh
