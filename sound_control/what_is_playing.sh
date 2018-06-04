91e186aa04a4c7234bd488bab1a3ffd0a3776d35
Nathan Harrington <harrington.nathan@gmail.com>
2018-06-04 14:12:19 -0400

    mplayer too

--- sound_control/what_is_playing.sh
+++ sound_control/what_is_playing.sh
@@ -9,11 +9,16 @@ AVAHINAME=192.168.0.70
 PORT=5577
 PASSWORD=cmuscontrolitifyouwant
 
+
 cmus-remote \
     --server $AVAHINAME:$PORT --passwd $PASSWORD -Q \
     | grep file \
     | osd_cat --align=center \
               --font=Variable --offset=450 \
-              --outline=35
+              --outline=35 &
 
+ps -aef | grep mplayer | grep mp3 | awk '{print $9}' \
+    | osd_cat --align=center \
+              --font=Variable --offset=550 \
+              --color=white --outline=35 &
 
