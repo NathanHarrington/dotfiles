f1a84cb537f064513429f7dd2eeff0ac0cc47d8b
Nathan Harrington <harrington.nathan@gmail.com>
2018-06-20 12:37:44 -0400

    rock list shuffle too

--- sound_control/classical_only.sh
+++ sound_control/classical_only.sh
@@ -3,10 +3,10 @@
 AVAHINAME=192.168.0.70
 PORT=5577
 PASSWORD=cmuscontrolitifyouwant
-QLIST=/home/nharrington/projects/dotfiles/sound_control/classical_cmus_queuelist.m3u
 
 FLD=/home/nharrington/projects/dotfiles/sound_control
 cat $FLD/classical_cmus_queuelist.m3u | shuf > $FLD/shuf_list.m3u
+
 cmus-remote --server $AVAHINAME:$PORT --passwd $PASSWORD -q $FLD/shuf_list.m3u
 cmus-remote --server $AVAHINAME:$PORT --passwd $PASSWORD --next
 
--- sound_control/rocklist_only.sh
+++ sound_control/rocklist_only.sh
@@ -5,7 +5,10 @@
 AVAHINAME=192.168.0.70
 PORT=5577
 PASSWORD=cmuscontrolitifyouwant
-QLIST=/home/nharrington/projects/dotfiles/sound_control/full_spectrum_music_queuelist.m3u
-cmus-remote --server $AVAHINAME:$PORT --passwd $PASSWORD -q $QLIST
+
+FLD=/home/nharrington/projects/dotfiles/sound_control
+cat $FLD/full_spectrum_music_queuelist.m3u | shuf > $FLD/shuf_list.m3u
+
+cmus-remote --server $AVAHINAME:$PORT --passwd $PASSWORD -q $FLD/shuf_list.m3u
 cmus-remote --server $AVAHINAME:$PORT --passwd $PASSWORD --next
 
