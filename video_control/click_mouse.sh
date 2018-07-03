10499a29e31da19e1dd211a57beddc9367db925e
Nathan Harrington <harrington.nathan@gmail.com>
2018-07-03 11:34:27 -0400

    click mouse

---
+++ video_control/click_mouse.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Conveniece function to click the mouse wherever the cursor is.
+#
+# This is designed for exiting modal windows and menu popups without
+# moving the hand to the mouse
+#
+# Map this to ctrl-alt-shift-m in gnome keyboard shortcuts
+xdotool click 1
