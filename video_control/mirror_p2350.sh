77b7bf24a44c2f539b506126a53ee470c5c8d4a6
Nathan Harrington <harrington.nathan@gmail.com>
2018-07-03 09:30:13 -0400

    more notes

--- video_control/mirror_p2350.sh
+++ video_control/mirror_p2350.sh
@@ -7,6 +7,19 @@
 # and a P2350 from about 2009?  The modeline below was generated with
 # the cvt command: cvt 1600 900
 #
+# The real problem this is resolution issues, workspace switching, and
+# plugging and unplugging of monitors when working as a laptop as
+# primary. What I want is a single external monitor where all of the
+# windows are in the exact same space with the same geometry and
+# workspace locations as the laptop. A true mirrored setup that survives
+# across new screen plug and unplug events.
+#
+# Required setup (one time):
+# Stop gnome from moving workspaces:
+#
+# gsettings set org.gnome.shell.overrides workspaces-only-on-primary false
+#
+#
 # Usage:
 # 1. Login to gnome.
 # 2. Use the hardware video display keys to mirror the desktop.
@@ -19,5 +32,16 @@
 # 5. Use the hardware video display keys again to mirror the desktop.
 # 6. You should now see a slightly blurry 1600x900 overscanned full
 #     screen usage on the external and native 1600x900 on the laptop
+#
+#
+# Verify this is working by creating windows on different workspaces,
+# then unplug the hdmi cable. Re-plug the cable and verify the windows
+# go back to their workspaces instead of all moving to workspace 1
+#
+
 xrandr --newmode "1600x900_60.00"  118.25  1600 1696 1856 2112  900 903 908 934 -hsync +vsync
 xrandr --addmode HDMI-1 1600x900_60.00
+
+#sleep 5
+#xrandr --output eDP-1 --same-as HDMI-1
+
